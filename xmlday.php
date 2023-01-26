73a1173dbd2aa34238853130459bf5735d5d00c2
Vincent DA CONCEICAO RAMOS <vince9322@gmail.com>
2023-01-26 11:34:36 +0100

    Added annotations

--- xmlday.php
+++ xmlday.php
@@ -9,21 +9,25 @@
   </head>
   <body>
     <div id="mainDivForm">
+      <!-- Form starts here -->
       <form action="" method="get" class="form">
         <div class="formText textFormat ">
           <div>
             <label for="pageLink">Merci d'entrer votre lien RSS ci-dessous</label>
           </div>
           <div>
+            <!-- The code below makes that if you enter a link, the link stays in the link bar till you delete it. Needed to pass it in the url. -->
             <?php if(isset($_GET['pageLink'])) {
               $urlValue = $_GET['pageLink'];
             } else {
               $urlValue = '';
             }
             ?>
+            <!-- end -->
             <input type="text" name="pageLink" id="pageLink" class="textFormat" value=<?= $urlValue ?>>
           </div>
         </div>
+        <!-- Default value for article number filter is 5, but if its more the code below get the number from url via GET and put it in the range input -->
         <div class="textFormat" id="numberChoice">
           <label for="articlesNumber">Nombre d'articles désirés : </label>
           <?php if(isset($_GET['articlesNumber'])) {
@@ -34,16 +38,25 @@
             ?>
           <input type="number" class="textFormat" id="articlesNumberInput" name="articlesNumber" min="5" max="100" step="5" value=<?= $numberValue  ?>>
         </div>
+        <!-- end -->
         <div class="formBtn">
           <input type="submit" value="C'est parti!" id="formBtn" class="textFormat">
         </div>
+        <!-- This is the pages numbers buttons which are generated depending on how much articles per page there is -->
         <?php if (isset($_GET['pageLink']) && isset($_GET['articlesNumber'])) {
           $url = $_GET['pageLink'];
           $xml = file_get_contents($url);
           $xml = simplexml_load_string($xml);
+          // intval function is there to get the number from the articlesNumber string
           $numberOfArticlesWantedInteger = intval($_GET['articlesNumber']);
+
+          // The numberOfPages variable is there to count how much page we need
           $numberOfPages = count($xml->channel->item) / $numberOfArticlesWantedInteger;
+          // Then we use numberOfPages to stop the loop when generating the pages buttons
           echo "<div id='pagesnumbers'>";
+          // In the for loop, we use two variables :
+          // $i to put the right value in the URL which is the one we gonna pass in the function later when we click and submit the page
+          // $k to DISPLAY the value inside the button since we are human and counting from page 1 and not 0
           for ($j = 0, $k = 1; $j < $numberOfPages; $j++, $k++) {
             echo "<div class='numberscards'>
             <button type='submit' value='$j' name='page' class='pagenumber'>$k</button>
@@ -53,8 +66,11 @@
           echo "</div>";
         }
           ?>
+        <!-- End of pages numbers buttons -->
       </form>
+      <!-- End of form -->
     </div>
+    <!-- The function div below is where all the articles are displayed -->
     <div id="xmlFunctionDiv">
       <?php
         require_once('xmlfunction.php');
@@ -74,5 +90,6 @@
           }
       ?>
     </div>
+    <!-- End of the function div -->
   </body>
 </html>
--- xmlfunction.php
+++ xmlfunction.php
@@ -3,15 +3,19 @@ function xmlToPage($url, $numberOfArticles, $page)
 {
   $xml = file_get_contents($url);
   $xml = simplexml_load_string($xml);
+  // If page is page 1 or there is no page number we are generating articles from the first one, this is why $i starts at 0
   if ($page === 1 || !$_GET['page']) {
     $i = 0;
     foreach ($xml->channel->item as $news) {
+      // Everytime an article is generated we increment $i
       $i++;
+      // Getting the article image
       $content = $news->children('media', true)->content;
       $contentattr = $content->attributes();
       if (isset($contentattr)) {
         $image = $contentattr["url"];
       }
+      // Then generating the article
       echo " <a href='$news->link'>";
       echo "<div id='card'>";
       if (isset($image)) {
@@ -22,17 +26,23 @@ function xmlToPage($url, $numberOfArticles, $page)
       echo "<div>$news->description</div>";
       echo "</div></div>";
       echo "</a>";
+      // The foreach loop will break if it reaches the number of articles we asked
       if ($i >= $numberOfArticles) {
         break;
       }
     }
   } else {
+    // $j is the start of our loop
+    // $i will be the index to know when we need to start displaying articles that's why it gets the $_GET['articlesNumber'],
+    // To know how many articles we did display in the previous page
     $j = 0;
     $i = $numberOfArticles * $page + 1;
     foreach ($xml->channel->item as $news) {
+      // We increment $j every time we go through one element in the array but we are not displaying them
       $j++;
       $content = $news->children('media', true)->content;
       $contentattr = $content->attributes();
+      // We start displaying the articles when $j reached $i which is the indicator of the last article from the previous page
       if ( $j >= $i ) {
         if (isset($contentattr)) {
           $image = $contentattr["url"];
@@ -47,6 +57,7 @@ function xmlToPage($url, $numberOfArticles, $page)
         echo "<div>$news->description</div>";
         echo "</div></div>";
         echo "</a>";
+        // If $j reach the maximum number of article for 1 page then the loop breaks
         if ( $j > $numberOfArticles * $page + $numberOfArticles - 1 ) {
           break;
         }
