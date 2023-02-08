36e92bb48cdb47cf09a49f5879fbf3d6b37af2e6
Vincent DA CONCEICAO RAMOS <vince9322@gmail.com>
2023-02-08 21:09:33 +0100

    footer and credits done

--- footer.php
+++ footer.php
@@ -1,3 +1,8 @@
 <div id="footer" class="textFormat">
-  <div> 
+  <div id='text'>
+    <div> e-Mail : Vince9322@gmail.com</div>
+    <div> Phone Number : 07 70 32 38 79 </div>
+    <div> Powered by vK </div>
+    <div> GitHub : vK-y0
+  </div>
 </div>
--- xmldisplay.php
+++ xmldisplay.php
@@ -181,8 +181,8 @@ error_reporting(E_ERROR | E_PARSE);
         </div>
         <!-- End of the function div -->
       </div>
+      <?php include('footer.php');?>
     </div>
-  <?php include('footer.php');?>
   </body>
   <!-- Javascript balise for night mode -->
   <script src="script.js"></script>
